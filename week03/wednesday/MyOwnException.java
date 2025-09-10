8cf3e879d694747b8017f9ba5f7d633b46440995
Jin An <jin.an@dtcc.edu>
2025-09-10 14:47:41 -0400

    Create MyOwnException.java

---
+++ week03/wednesday/MyOwnException.java
@@ -0,0 +1,12 @@
+public class MyOwnException extends Throwable {
+}
+
+class Bahbah {
+    public static void main(String[] args) {
+
+    }
+
+    static void mahaamaha() throws MyOwnException {
+
+    }
+}
