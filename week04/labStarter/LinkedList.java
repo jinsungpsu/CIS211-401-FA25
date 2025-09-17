675403b2f1b068b043dc4310a92025978070ed04
Jin An <jin.an@dtcc.edu>
2025-09-17 14:41:43 -0400

    Create LinkedList.java

---
+++ week04/labStarter/LinkedList.java
@@ -0,0 +1,53 @@
+public class LinkedList<T> {
+    private Node head;
+    private int count = 0;
+
+    public void add(T item) {
+        Node node = new Node(item, null);
+        head = node;
+        count++;
+    }
+
+    public T get(int index) throws IndexOutOfBoundsException {
+        if (index < 0 || index > count) {
+            throw new IndexOutOfBoundsException("Doh");
+        } else {
+            // iterate through the linked list
+            // condition is a little different than what we did in class
+            // now it's a counter based loop (aka for loop)
+            // iterate the same way...
+            /*
+            Node itr = head;
+            for () {
+                itr = itr.next;
+            }
+            return itr.data;
+             */
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder output = new StringBuilder();
+        if (count == 0) return "[ ]";
+        output.append("[");
+        // loop through WHOLE LinkedList
+        // meaning until you hit the tail
+        // inside the loop
+        // output.append(itr.data + " ");
+
+        output.append("]");
+        return output.toString();
+    }
+
+    // nested/helper class
+    private class Node {
+        T data;
+        Node next;
+
+        public Node(T data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+}
