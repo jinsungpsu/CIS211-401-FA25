c8b1e12e1fda0aa44a12fb4f3ad69e9c68ddb941
Jin An <jin.an@dtcc.edu>
2025-09-22 14:44:13 -0400

    Create Main.java

---
+++ week5/monday/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Stack<Integer> numStack = new LinkedStackA<>();
+        int i = 0;
+        while(true) {
+            numStack.push(i++);
+        }
+//        System.out.println(numStack.peek());
+//
+//        java.util.List<Integer> stuff = new java.util.LinkedList<>();
+//        stuff.add(1);
+//        stuff.add(2);
+//        stuff.add(3);
+//        System.out.println(stuff);
+//        stuff.add(2, 99);
+    }
+}
